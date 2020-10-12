ba35146f055f0be38fcdaea9dc07070ea7f3a907
Trent Douglas <trentdouglas@mymail.mines.edu>
2020-10-12 15:08:27 -0600

    almost done...

--- Test_algo.py
+++ Test_algo.py
@@ -1,7 +1,7 @@
 import math
 import statistics
-import numpy as np
-import matplotlib.pyplot as plt
+#import numpy as np
+#import matplotlib.pyplot as plt
 from operator import attrgetter
 class subsets_and_averages:
     def __init__(self, subset, average, average_point):
@@ -9,7 +9,29 @@ class subsets_and_averages:
         self.average = average
         self.average_point = average_point
 
-testInput = [[1,1],[-3,2],[9,-5],[3,-6],[-2,0],[-4,-6],[-1,-1],[0,0],[-7,5],[-1,9], [15,2],[-13,7],[-9,-5],[23,-10],[-12,32],[24,-5],[-6,-6],[9,-12],[-10,5],[-7,2]]
+
+f = open("10_points.txt")
+testInput = []
+n = int(f.readline())
+k = int(f.readline())
+points = []
+for x in f:
+  point = []
+  input = x.split()
+  point.append(int(input[0]))
+  point.append(int(input[1]))
+  point.append(int(input[2]))
+
+  points.append(point)
+
+f.close()
+
+# print(n)
+# print(k)
+# for i in points:
+#     print(i)
+
+testInput = points
 
 def powerSet(items):
 
@@ -25,9 +47,13 @@ def powerSet(items):
     return newList
 
 def removenullSetAndSingles(allCombinations):
-    for i in allCombinations:
-        if(len(i) == 0 or len(i) == 1):
-            allCombinations.remove(i);
+    allCombinations.remove([])
+    counter = 0
+    while (counter != len(allCombinations)-1):
+        if(len(allCombinations[counter]) == 1):
+            allCombinations.remove(allCombinations[counter])
+            counter -= 1
+        counter += 1
     return allCombinations;
 
 def calcComboDistances(allPairsOrMore):
@@ -41,16 +67,20 @@ def calcComboDistances(allPairsOrMore):
 def calcAvgDistance(subset):
     xs = []
     ys = []
+    zs = []
     x_distances = []
     y_distances = []
+    z_distances = []
     euclidian_distances = []
     for i in subset:
         xs.append(i[0])
         ys.append(i[1])
+        zs.append(i[2])
 
     average_x = statistics.mean(xs)
     average_y = statistics.mean(ys)
-    average_point = [average_x, average_y]
+    average_z = statistics.mean(zs)
+    average_point = [average_x, average_y, average_z]
 
     for i in range(len(subset)):
         x = subset[i][0]
@@ -60,8 +90,14 @@ def calcAvgDistance(subset):
         y = subset[i][1]
         for j in ys:
             y_distances.append(abs(y - j))
+
+        z = subset[i][2]
+        for j in zs:
+            z_distances.append(abs(z - j))
+
+
     for i in range(len(x_distances)):
-        euclidian_distances.append(math.sqrt(pow(x_distances[i],2) + pow(y_distances[i], 2)))
+        euclidian_distances.append(math.sqrt(pow(x_distances[i],2) + pow(y_distances[i], 2) + pow(z_distances[i], 2)))
 
     average_distance = statistics.mean(euclidian_distances)
 
@@ -102,12 +138,15 @@ def add_leftovers(list_of_papas, leftovers):
     for leftover in leftovers:
         leftover_x = leftover[0]
         leftover_y = leftover[1]
+        leftover_z = leftover[2]
         for papa in list_of_papas:
             papa_point_x = papa.average_point[0]
             papa_point_y = papa.average_point[1]
+            papa_point_z = papa.average_point[2]
             x_difference = abs(leftover_x - papa_point_x)
             y_difference = abs(leftover_y - papa_point_y)
-            euclidian_distance = math.sqrt(pow(x_difference,2) + pow(y_difference, 2))
+            z_difference = abs(leftover_z - papa_point_z)
+            euclidian_distance = math.sqrt(pow(x_difference,2) + pow(y_difference, 2) + pow(z_difference, 2))
             if(euclidian_distance < min_distance):
                 min_distance = euclidian_distance
                 papa_group = papa
@@ -115,9 +154,6 @@ def add_leftovers(list_of_papas, leftovers):
 
     return list_of_papas
 
-
-k = input ("pick k: ")
-k = int(k)
 
 powerSet = powerSet(testInput)
 clean_power_set = removenullSetAndSingles(powerSet)
@@ -127,9 +163,10 @@ list_of_papas = pickPapas(k, sorted_clean_power_set_with_distances)
 leftovers = removeChosenPoints(testInput, list_of_papas)
 finalSet = add_leftovers(list_of_papas, leftovers)
 
-for i in leftovers:
-    print(i)
 
-# for i in finalSet:
-#     print(i.subset)
+for papa in list_of_papas:
+    print(papa.average_point)
+
+for i in finalSet:
+    print(i.subset)
 
